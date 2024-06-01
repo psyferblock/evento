5037a5ba57c82977e0bfe8c74e08a4d65368bb9c
psyfer <psyferblock.io@gmail.com>
2024-06-01 07:58:56 +0300

    header setup

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-  return (
-    <header className='w-full'><div className="wrapper"></div></header>
-  )
-}
+	return (
+		<header className="w-full">
+			<div className="wrapper flex items-center justify-between">
+				<Link
+					href="/"
+					className="w-36"
+				>
+					<Image
+						src="/assets/images/logo.svg"
+						width={128}
+						height={38}
+						alt="Evento Logo"
+					/>
+				</Link>
+                <div className="flex w-32 justify-end gap-3">
+                    
+                </div>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
