c50f282c32eb0f97b2d4acd73016db8c211d4c18
psyfer <psyferblock.io@gmail.com>
2024-06-07 14:30:12 +0300

    order model with types

--- src/lib/database/models/order.model.ts
+++ src/lib/database/models/order.model.ts
@@ -1,10 +1,47 @@
 import { Schema, model, models } from "mongoose";
 
+export interface IOrder extends Document {
+    createdAt: Date
+    stripeId: string
+    totalAmount: string
+    event: {
+      _id: string
+      title: string
+    }
+    buyer: {
+      _id: string
+      firstName: string
+      lastName: string
+    }
+  }
+  export type IOrderItem = {
+    _id: string
+    totalAmount: string
+    createdAt: Date
+    eventTitle: string
+    eventId: string
+    buyer: string
+  }
 const orderSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
   },
+  stripeId:{
+    type:String,
+    required:true,
+    unique:true,
+  },
+  totalAmmount:{
+    type:Schema.Types.ObjectId,
+    ref:"Event",
+  },
+  buyer:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+
+  }
+
 });
 
 const Order = models.Order || model("Order", orderSchema);
