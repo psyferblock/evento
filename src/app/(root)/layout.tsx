8d9869421233006a068a0e78f71fdb7bff7f1c2f
psyfer <psyferblock.io@gmail.com>
2024-06-07 20:11:11 +0300

    auth layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 		<>
 			<div className="flex flex-col h-screen">
 				<Header />
-				<main className="flex-1">{children}</main>
+				<main className="flex-1 flex">{children}</main>
 				<Footer />
 			</div>
 		</>
