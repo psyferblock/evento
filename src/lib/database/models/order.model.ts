bc3909847b6e778940b6fe1dbf87dc5474662ce6
psyfer <psyferblock.io@gmail.com>
2024-06-07 13:49:54 +0300

    clerk webhooks and user apis

---
+++ src/lib/database/models/order.model.ts
@@ -0,0 +1,11 @@
+import { Schema, model, models } from "mongoose";
+
+const orderSchema = new Schema({
+    
+})
+
+
+const Order = models.Urder || model("user", orderSchema);
+
+
+export const Order
