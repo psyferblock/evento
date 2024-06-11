08d2ab8df234a02db985701476b46f30d4fea7ed
psyfer <psyferblock.io@gmail.com>
2024-06-11 12:42:52 +0300

    struggling iwth mongoose connection

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,23 +1,42 @@
+// lib/mongodb.js
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-// initializing a cached variable
-// attempting to retrieve a mongoose property from the global object.
-// gglobal as any will create the type of cached from mongoose itself
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connectToDatabase = async () => {
-  // checking if cached is already connecting
-  if (cached.conn) return cached.conn;
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
 
-  //here we connect to an existing connection or create a new one
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      dbName: "cluster0",
-      bufferCommands: false,
-    });
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "Cluster0",
+        bufferCommands: false,
+      })
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
   cached.conn = await cached.promise;
   return cached.conn;
-};
+}
+
+export default connectToDatabase;
