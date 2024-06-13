9a2c360158238d36330dddcecd23c11133721b3b
psyfer <psyferblock.io@gmail.com>
2024-06-13 13:15:54 +0300

    path for webhooks

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,73 +1,53 @@
-import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+import mongoose from "mongoose";
+declare global {
+  var mongoose: any; // This must be a `var` and not a `let / const`
+}
+console.log("MONGODB_URI", process.env.MONGODB_URI_DEVELOPMENT);
+console.log("MONGODB_URI", process.env.MONGODB_URI_PRODUCTION);
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+const MONGODB_URI =
+  process.env.NODE_ENV === "development"
+    ? 
+    process.env.MONGODB_URI!
+    : process.env.MONGODM_URI_PRODUCTION!;
 
-export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+console.log("MONGODB_URI", MONGODB_URI);
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'eventus',
-    bufferCommands: false,
-  })
+let cached = global.mongoose;
 
-  cached.conn = await cached.promise;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      // dbName: "eventus",
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+    console.log("newlyConnected", cached.conn);
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
 
   return cached.conn;
 }
-
-// import mongoose from "mongoose";
-// declare global {
-//   var mongoose: any; // This must be a `var` and not a `let / const`
-// }
-// console.log("MONGODB_URI", process.env.MONGODB_URI_DEVELOPMENT);
-// console.log("MONGODB_URI", process.env.MONGODB_URI_PRODUCTION);
-
-// const MONGODB_URI =
-//   // process.env.NODE_ENV === "development"
-//     // ? 
-//     process.env.MONGODB_URI_DEVELOPMENT!
-//     // : process.env.MONGODM_URI_PRODUCTION!;
-
-// console.log("MONGODB_URI", MONGODB_URI);
-
-// if (!MONGODB_URI) {
-//   throw new Error(
-//     "Please define the MONGODB_URI environment variable inside .env.local",
-//   );
-// }
-
-// let cached = global.mongoose;
-
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
-
-// async function connectToDatabase() {
-//   if (cached.conn) {
-//     return cached.conn;
-//   }
-//   if (!cached.promise) {
-//     const opts = {
-//       bufferCommands: false,
-//       // dbName: "eventus",
-//     };
-//     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-//       return mongoose;
-//     });
-//   }
-//   try {
-//     cached.conn = await cached.promise;
-//     console.log("newlyConnected", cached.conn);
-//   } catch (e) {
-//     cached.promise = null;
-//     throw e;
-//   }
-
-//   return cached.conn;
-// }
 
 // export default connectToDatabase;
