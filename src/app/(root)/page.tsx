5bf1748c1f6cd24af279dc463510076e98e8fd7c
psyfer <psyferblock.io@gmail.com>
2024-06-20 16:16:31 +0300

    pagination and query

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -64,8 +64,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
         emptyStateSubtext="Come Back Later"
         collectionType="All_Events"
         limit={6}
-        page={1}
-        totalPages={2}
+        page={page}
+        totalPages={events?.pages}
       />
     </>
   );
